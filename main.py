eafcb420da733634bf40c4b52caf7b7f29c194c5
Daniel Noshadha <daniel@knu.ua>
2023-03-29 08:22:07 +0200

    Function word_counter

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ filename = input("Введіть назву файлу: ")
 with open(filename, 'r') as file:
     text = file.read()
 
+# визначаємо кількість слів
+def word_counter():
+    words = re.findall(r'\b\w+\b', text)
+    num_words = len(words)
+    print("Кількість слів у файлі:", num_words)
+
+
+word_counter()
