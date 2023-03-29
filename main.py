df08dd0e48e4544e5aca7e78d2e4d2ed29137797
Daniel Noshadha <daniel@knu.ua>
2023-03-29 08:38:35 +0200

    Added function and unit tests to all function
    Instead of regular file input, made function for this

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import re
 
-filename = input("Введіть назву файлу: ")
-
-with open(filename, 'r') as file:
-    text = file.read()
+def file_text():
+    filename = "text.txt"
+    with open(filename, 'r') as file:
+        text = file.read()
+    return text
 
 # визначаємо кількість слів
-def word_counter():
+def word_counter(text):
     words = re.findall(r'\b\w+\b', text)
     num_words = len(words)
-    print("Кількість слів у файлі:", num_words)
+    return print("Кількість слів у файлі:", num_words)
+
+# визначаємо кількість речень
+def sentences_counter(text):
+    sentences = re.findall(r'[\w\s]*[\.\?!]', text)
+    num_sentences = len(sentences)
+    return print("Кількість речень у файлі:", num_sentences)
 
 
-word_counter()
+
+word_counter(file_text())
+sentences_counter(file_text())
---
+++ unit_test.py
@@ -0,0 +1,37 @@
+import pytest
+import re
+
+# фікстура для зчитування вхідних даних з файлу
+@pytest.fixture()
+def file_text():
+    filename = "test_file.txt"
+    with open(filename, 'r') as file:
+        text = file.read()
+    return text
+
+# параметризований тест для функції word_counter
+@pytest.mark.parametrize("text, expected", [
+    ("Hello, world!", 2),
+    ("This is a test.", 4),
+    ("   ", 0)
+])
+def test_word_counter(text, expected):
+    def word_counter(text):
+        words = re.findall(r'\b\w+\b', text)
+        num_words = len(words)
+        return num_words
+    assert word_counter(text) == expected
+
+# параметризований тест для функції sentences_counter
+@pytest.mark.parametrize("text, expected", [
+    ("Hello, world!", 1),
+    ("This is a test.", 1),
+    ("This is a test. This is only a test.", 2),
+    ("   ", 0)
+])
+def test_sentences_counter(text, expected):
+    def sentences_counter(text):
+        sentences = re.findall(r'[\w\s]*[\.\?!]', text)
+        num_sentences = len(sentences)
+        return num_sentences
+    assert sentences_counter(text) == expected
